c93dc81907eb5a7fb34c8abce2b15bdade5e4403
yuihan <juyi@ualberta.ca>
2023-04-29 09:29:45 -0600

    top banner with title

--- recyclo/lib/widgets/item_list.dart
+++ recyclo/lib/widgets/item_list.dart
@@ -7,41 +7,85 @@ class ItemList extends StatelessWidget {
   const ItemList({super.key});
 
   Widget createListWidget(List<Item> items) {
+    //final double width = MediaQuery.of(context.size.height);
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        for (var i in items)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Image.asset('assets/images/${i.category}.png'),
-                ),
-              ),
-              SizedBox(
-                height: 40,
-                width: 200,
-                child: Text(
-                  i.itemName,
-                  style: const TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              SizedBox(
-                height: 40,
-                width: 200,
-                child: Text(
-                  i.category,
-                  style: const TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(50),
+            ),
+            color: Colors.green,
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 80),
+              Container(
+                height: 80,
+                width: 1000,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ), // border only
+                ), // box decoration
+                child: Center(
+                  child: Text(
+                    "ReCyclo",
+                    style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.green,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ],
-          )
+          ),
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                for (var i in items)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SizedBox(
+                          height: 100,
+                          width: 100,
+                          child: Image.asset('assets/images/${i.category}.png'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 200,
+                        child: Text(
+                          i.itemName,
+                          style: const TextStyle(fontSize: 25),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 200,
+                        child: Text(
+                          i.category,
+                          style: const TextStyle(fontSize: 25),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  )
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
